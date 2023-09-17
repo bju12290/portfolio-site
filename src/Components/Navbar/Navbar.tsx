d5727d3c07217364810d595ca29fb5b17ba502cd
BuildTools <unconfigured@null.spigotmc.org>
2023-09-16 22:19:31 -0400

    Updated Nav Links

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,36 +3,41 @@ import './Navbar.css'
 
 
 export default function Navbar() {
-    // When NavBar Reaches x Position, Collapse It to the Left
-    // At the same position, make the Hamburger Menu Visible
 
     React.useEffect(() => {
         window.addEventListener('scroll', () => {
-            const topNavbar = document.querySelector('.top--navbar--container') as HTMLElement | null;
-            const sideMenu = document.querySelector('.menuToggle') as HTMLElement | null;
-            const scrollPos = window.scrollY;
-            const transitionPosition = 200; // Adjust this value as needed
+            const topNavbar = document.querySelector('.top--navbar--container') as HTMLElement | null
+            const sideMenu = document.querySelector('.menuToggle') as HTMLElement | null
+            const scrollPos = window.scrollY
+            const transitionPosition = 200
           
             if (topNavbar && sideMenu) {
               if (scrollPos >= transitionPosition) {
-                topNavbar.classList.add('hidden');
+                topNavbar.classList.add('hidden')
                 sideMenu.classList.remove('hidden')
               } else {
-                topNavbar.classList.remove('hidden');
+                topNavbar.classList.remove('hidden')
                 sideMenu.classList.add('hidden')
               }
             }
-          });
+          })
     })
+
+    function scrollToSection(sectionId: string) {
+        const element = document.getElementById(sectionId);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' })
+        }
+      }
     
     return (
         <div className="navbar--container">
             <div className="top--navbar--container">
                 <div className="navbar navbar--content">
                     Brian Hartnett <span className="navbar--copyright">&copy; 2023</span>
-                    <a href="/#About">About</a>
-                    <a href="/#Portfolio">Portfolio</a>
-                    <a href="/#Contact">Contact</a>
+                    <a href="#About" onClick={() => scrollToSection('About')}>About</a>
+                    <a href="#Portfolio" onClick={() => scrollToSection('Portfolio')}>Portfolio</a>
+                    <a href="#Contact" onClick={() => scrollToSection('Contact')}>Contact</a>
                 </div>
             </div>
             <div className="menuToggle hidden">
@@ -41,10 +46,10 @@ export default function Navbar() {
                     <span></span>
                     <span></span>
                     <ul className="menu">
-                        <a href="#"><li>Back to Top</li></a>
-                        <a href="#About"><li>About</li></a>
-                        <a href="#Portfolio"><li>Portfolio</li></a>
-                        <a href="#Contact"><li>Contact</li></a>
+                        <a href="#" onClick={() => scrollToSection('')}><li>Back to Top</li></a>
+                        <a href="#About" onClick={() => scrollToSection('About')}><li>About</li></a>
+                        <a href="#Portfolio" onClick={() => scrollToSection('Portfolio')}><li>Portfolio</li></a>
+                        <a href="#Contact" onClick={() => scrollToSection('Contact')}><li>Contact</li></a>
                     </ul>    
             </div>
         </div>
